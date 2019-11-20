c0c216292f624b83278c6c4a7f7b2139552b0464
Mustapha <mustapha.kadri66@gmail.com>
2019-11-20 18:29:24 +0100

    configuration de webpack encore et de React

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+require('../css/app.css');
+
+
+const App = () => {
+    return <h1> Bonjour à tous </h1>;
+}
+
+
+const rootElement = document.querySelector('#app');
+ReactDOM.render(<App/>, rootElement);
